4e148355b9938d18913608429b1f84db49e9d208
minahYu <mina010311@naver.com>
2023-12-12 15:37:32 +0900

    feat : 유저 엔티티 추가

--- src/main/java/com/example/market/domain/user/entity/User.java
+++ src/main/java/com/example/market/domain/user/entity/User.java
@@ -1,4 +1,17 @@
 package com.example.market.domain.user.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
 }
