e9ffa553caa13bfd3aa26777821e147b45ea08af
minahYu <mina010311@naver.com>
2023-12-12 18:09:29 +0900

    fix : 비밀번호 닉네임 포함 체크 메서드 수정

--- src/main/java/com/example/market/domain/user/controller/UserController.java
+++ src/main/java/com/example/market/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.market.domain.user.dto.request.SignupRequestDto;
 import com.example.market.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +19,12 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@Valid @RequestBody SignupRequestDto requestDto) {
-        userService.signup(requestDto);
-
-        return ResponseEntity.ok().build();
+        boolean result = userService.signup(requestDto);
+        
+        if(result) {
+            return ResponseEntity.ok().body("회원가입이 완료되었습니다");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("중복된 닉네임입니다.");
+        }
     }
 }
--- src/main/java/com/example/market/domain/user/service/UserService.java
+++ src/main/java/com/example/market/domain/user/service/UserService.java
@@ -13,19 +13,21 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
 
-    public void signup(SignupRequestDto requestDto) {
+    public boolean signup(SignupRequestDto requestDto) {
         String nickname = requestDto.getNickname();
         String password = requestDto.getPassword();
         String passwordCheck = requestDto.getPasswordCheck();
         Optional<User> user = userRepository.findByNickname(requestDto.getNickname());
 
         if (user.isPresent()) {
-            throw new IllegalArgumentException("중복된 닉네임입니다.");
+            //throw new IllegalArgumentException("중복된 닉네임입니다.");
+            return false;
         } else {
-            if (checkSamePassword(password, passwordCheck) && checkPasswordNotContainsUsername(password, nickname)) {
+            if (checkSamePassword(password, passwordCheck) && checkPasswordNotContainsNickname(password, nickname)) {
                 User newUser = new User(nickname, password);
                 userRepository.save(newUser);
             }
+            return true;
         }
     }
 
@@ -41,12 +43,12 @@ public class UserService {
     }
 
     /**
-     * password에 username이 포함되어있지 않은지 확인하는 메서드
+     * password에 nickname이 포함되어있지 않은지 확인하는 메서드
      */
-    private boolean checkPasswordNotContainsUsername(String username, String password) {
-        if (password.contains(username))
+    private boolean checkPasswordNotContainsNickname(String nickname, String password) {
+        if (!password.contains(nickname))
             return true;
         else
-            throw new IllegalArgumentException("password에는 username과 같은 값이 포함될 수 없습니다.");
+            throw new IllegalArgumentException("password에는 nickname과 같은 값이 포함될 수 없습니다.");
     }
 }
