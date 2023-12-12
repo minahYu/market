61b68e3319487bc776192121f273ab168370b4b6
minahYu <mina010311@naver.com>
2023-12-12 16:19:43 +0900

    feat : 유저서비스에 비밀번호 확인하는 메서드 추가

--- src/main/java/com/example/market/domain/user/service/UserService.java
+++ src/main/java/com/example/market/domain/user/service/UserService.java
@@ -19,8 +19,21 @@ public class UserService {
         if(user.isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 회원입니다.");
         } else {
-            User newUser = new User(requestDto.getUsername(), requestDto.getPassword());
-            userRepository.save(newUser);
+            if(checkSamePassword(requestDto.getPassword(), requestDto.getPasswordCheck())) {
+                User newUser = new User(requestDto.getUsername(), requestDto.getPassword());
+                userRepository.save(newUser);
+            }
+        }
+    }
+
+    /**
+     * password와 passwordCheck가 같은 값인지 확인하는 메서드
+     */
+    private boolean checkSamePassword(String password, String passwordCheck) {
+        if(password.matches(passwordCheck)) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("입력하신 비밀번호와 일치하지 않습니다");
         }
     }
 }
