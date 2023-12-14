adb3575544e0ab2360e87652945d1c57267dbc58
minahYu <mina010311@naver.com>
2023-12-14 20:34:35 +0900

    feat : 요청받은 인증에 따른 결과를 리턴해주는 메서드 추가

--- src/main/java/com/example/market/domain/user/config/WebSecurityConfig.java
+++ src/main/java/com/example/market/domain/user/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.market.domain.user.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,5 +19,17 @@ public class WebSecurityConfig {
         // BCryptPasswordEncoder() : BCrypt해싱함수를 이용하는 PasswordEncoder로, 암호화를 해준다.
         //(BCrypt - 높은 보안성을 제공하며, 해시에 솔트(salt)를 추가하는 기능이 내장되어 있어 안전)
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 요청 받은 인증에 따른 결과를 리턴해주는 메서드
+     */
+    @Bean
+    // AuthenticationManager : 인증 요청을 처리하는 인터페이스로, authenticate 메서드를 가짐.
+    public AuthenticationManager authenticationManager(
+            // AuthenticationConfiguration : 인증 구성을 내보내는 클래스
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager(); // 인증에 성공하면 인증 정보를 받아와 리턴.
     }
 }
