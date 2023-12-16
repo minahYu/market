8962ea2755493e98ab18f6195f4e8fc737f729f8
minahYu <mina010311@naver.com>
2023-12-17 02:37:54 +0900

    feat : WebSecurityConfig에 필터 등록

--- src/main/java/com/example/market/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/market/global/config/WebSecurityConfig.java
@@ -1,5 +1,10 @@
 package com.example.market.global.config;
 
+import com.example.market.global.jwt.JwtUtil;
+import com.example.market.global.security.JwtAuthenticationFilter;
+import com.example.market.global.security.JwtAuthorizationFilter;
+import com.example.market.global.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +16,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Spring Security 설정을 담당하는 클래스
  */
 @EnableWebSecurity // : Spring Security를 기본 설정을 활성화하기 위한 annotation.
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    // AuthenticationConfiguration : 인증 구성을 내보내는 클래스
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean // :외부 라이브러리를 Spring이 관리하도록 등록해주는 annotation
     // PasswordEncoder : 스프링 시큐리티에서 비밀번호를 안전하게 저장할 수 있도록 비밀번호의 단방향 암호화를 지원하는 인터페이스.
@@ -37,6 +49,24 @@ public class WebSecurityConfig {
             AuthenticationConfiguration configuration
     ) throws Exception {
         return configuration.getAuthenticationManager(); // configuration에서 AuthenticationManager를 가져와 리턴
+    }
+
+    /**
+     * 인증 필터 등록
+     */
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    /**
+     * 인가 필터 등록
+     */
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() throws Exception {
+        return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
     }
 
 
@@ -66,6 +96,10 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/users/**").permitAll() // 해당 경로로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
+
+        // 필터 관리. 첫번째 인자 실행 전에 두번째 인자가 실행됨.
+        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         // HttpSecurity 객체에 설정된 보안 설정을 적용하고, 설정된 SecurityFilterChain 객체를 반환
         return http.build();
