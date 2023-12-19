817d81cdb8cd407d4740c59e98abb4c3b3461e55
minahYu <mina010311@naver.com>
2023-12-19 20:04:44 +0900

    feat : 댓글 컨트롤러에 응답메시지 관련 코드 추가

--- src/main/java/com/example/market/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/market/domain/comment/controller/CommentController.java
@@ -1,28 +1,30 @@
 package com.example.market.domain.comment.controller;
 
 import com.example.market.domain.comment.dto.CommentRequestDto;
+import com.example.market.domain.comment.dto.response.CommentResponseDto;
 import com.example.market.domain.comment.service.CommentService;
 import com.example.market.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comments")
 public class CommentController {
     private final CommentService commentService;
 
-    @PostMapping("/{id}")
-    public void createComment(
+    @PostMapping("/{postId}")
+    public ResponseEntity<?> createComment(
             @RequestBody CommentRequestDto requestDto,
             @PathVariable Long postId,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        commentService.createComment(requestDto, postId, userDetails.getUser());
+        CommentResponseDto responseDto = commentService.createComment(requestDto, postId, userDetails.getUser());
+        if(responseDto != null) {
+            return ResponseEntity.status(200).body("댓글을 등록하였습니다.");
+        }
+        return ResponseEntity.status(401).body("댓글을 등록할 수 없습니다.");
     }
 }
