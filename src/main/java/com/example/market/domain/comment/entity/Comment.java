1b94b4726c358a913205abb6c316ae0e301a0cba
minahYu <mina010311@naver.com>
2023-12-19 16:09:44 +0900

    feat : 댓글 엔티티 생성

---
+++ src/main/java/com/example/market/domain/comment/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.example.market.domain.comment.entity;
+
+import com.example.market.domain.model.BaseEntity;
+import com.example.market.domain.post.entity.Post;
+import com.example.market.domain.user.entity.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
